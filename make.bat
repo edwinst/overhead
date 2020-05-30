42adc0e98c2ff72dc0605df609fb6dfbffb02070
Edwin Steiner <edwin.steiner@gmx.net>
2020-05-30 11:44:17 +0200

    Add explanatory comment to make.bat.

--- make.bat
+++ make.bat
@@ -1,4 +1,7 @@
 @echo off
+rem This batch file is intended to be used from vim with 'set makeprg=cmd\ /c\ make.bat'
+rem You will need to adapt the path below to your environment.
+
 call D:\Programs\VS_IDE\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
 
 set DIR=%~dp0
